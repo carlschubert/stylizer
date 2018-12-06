3eca1637b49a81d0090949f0447b2ef1baba0820
carlschubert <carl.schubert@gmail.com>
2018-12-06 12:53:33 -0500

    added route for serving images from output directory

--- server/server.py
+++ server/server.py
@@ -1,27 +1,36 @@
 import os
-from flask import Flask, render_template, request, jsonify
-
 import subprocess
 
-app = Flask(__name__, static_folder="../static/dist", template_folder="../static/dist")
+from flask import Flask, render_template, request, jsonify, send_from_directory
+
+SERVER_DIR = "../server"
+STATIC_DIR = "../static"
+
+app = Flask(__name__, static_folder=os.path.join(STATIC_DIR, "dist"), template_folder=STATIC_DIR)
 
 CMD = """arbitrary_image_stylization_with_weights \
-  --checkpoint=$(pwd)/model/model.ckpt \
-  --output_dir=$(pwd)/output \
-  --style_images_paths=images/style_images/*.jpg \
-  --content_images_paths=images/content_images/*.jpg \
+  --checkpoint={server_dir}/model/model.ckpt \
+  --output_dir={static_dir}/dist/output \
+  --style_images_paths={server_dir}/images/style_images/*.jpg \
+  --content_images_paths={server_dir}/images/content_images/*.jpg \
   --image_size=256 \
   --content_square_crop=False \
   --style_image_size=256 \
   --style_square_crop=False \
-  --interpolation_weights=[0.0,0.2,0.4,0.6,0.8,1.0]
-  --logtostderr"""
+  --interpolation_weights=[0.0,0.2,0.4,0.6,0.8,1.0] \
+  --logtostderr""".format(server_dir=SERVER_DIR, static_dir=STATIC_DIR)
 
 def run_script():
     subprocess.call(CMD, shell=True)
 
+@app.route('/output/<path:filename>')
+def output(filename):
+  
+  return send_from_directory(os.path.join(app.static_folder, 'output/'), filename)
+
 @app.route("/", methods=['GET', 'POST'])
 def main():
+    app.logger.debug(app.static_folder)
     # TODO form validaton
       # * must be jpeg
       # * must be secure url
@@ -30,15 +39,16 @@ def main():
     if request.method == 'POST':
       style_images = request.files.getlist('styleBin')
       for i, im in enumerate(style_images):
-        im.save(os.path.join(os.getcwd(), "images", "style", im.name ))
+        im.save(os.path.join(os.getcwd(), "../server/images", "style_images", im.filename ))
 
       content_images = request.files.getlist('contentBin')
       for i, im in enumerate(content_images):
-        im.save(os.path.join(os.getcwd(), "images", "content", im.name ))
-     
+        im.save(os.path.join(os.getcwd(), "../server/images", "content_images", im.filename ))
 
+      run_script()
+
+      # output_urls = os.listdir()
      
-      
       return jsonify(canitacceptboolean=True)
     else:
       return render_template("index.html")
